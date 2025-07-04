8fef37c9b0f78f901820757d8f42c3268d9d253e
saman <sam.karimi727@gmail.com>
2025-07-05 01:08:06 +0330

    update 2 files and create 2 files

--- routes/digitalRouter.js
+++ routes/digitalRouter.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import getAllCategoriesController from '../controller/digital/getAllCategories.js'
+import filteringDigitalProducts from '../controller/digital/getDigitalsByCategory.js'
 
 
 const digitalRouter = express.Router()
 
 digitalRouter.get('/categories' , getAllCategoriesController)
+digitalRouter.get('/filtering' , filteringDigitalProducts)
 
 
 export default digitalRouter
