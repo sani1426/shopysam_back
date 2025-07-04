bd90f52ed43999b29ce1d70252624cbfcda3f0e0
saman <sam.karimi727@gmail.com>
2025-07-04 23:42:04 +0330

    create 1 file, delete 1 file and update 1 file

--- routes/clothRouter.js
+++ routes/clothRouter.js
@@ -1,8 +1,9 @@
 import express from 'express'
+import getAllClothCategories from '../controller/Cloth/getAllCategories.js'
 
 const clothRouter = express.Router()
 
-
+clothRouter.get('/categories' , getAllClothCategories)
 
 
 export default clothRouter
