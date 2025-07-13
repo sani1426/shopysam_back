a67fbc6ed64dc844a6994725b5ab861957c3e5f1
saman <sam.karimi727@gmail.com>
2025-07-14 01:48:02 +0330

    update verifyEmailTemplates.js

--- utils/verifyEmailTemplates.js
+++ utils/verifyEmailTemplates.js
@@ -1,9 +1,9 @@
 
 const verifyEmailTemplate = (name , url) => {
         return `
-        <p>Dear ${name}</p>
+        <p>Dear {${name}}</p>
         <h3>Thank you for registering in Shopysam</h3>
-        <a href=${url} style="color:white;background:green;margin-top:20px;padding:8px 20px>
+        <a href={${url}}  style="color:white;background:green;margin-top:20px;padding:8px 20px>
         Verify Email</a>
         `
 }
