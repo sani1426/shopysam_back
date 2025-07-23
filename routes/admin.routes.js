03e39136d1da72dc4ed3abf64c97be77887689cb
saman <sam.karimi727@gmail.com>
2025-07-24 02:29:05 +0330

    update admin.routes.js

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import Admin from '../middlewares/Admin.js'
 import { getAllUsersController } from '../controller/admin.controller.js'
+import verifyToken from '../middlewares/verifyToken.js'
 
 
 const adminRouter = express.Router()
 
-adminRouter.get('/all-users' , Admin, getAllUsersController)
+adminRouter.get('/all-users' ,verifyToken, Admin, getAllUsersController)
 
 export default adminRouter
