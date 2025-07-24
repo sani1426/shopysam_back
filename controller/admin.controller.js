313e331bf137a2f7c57d7b9ceef51c3472cf898e
saman <sam.karimi727@gmail.com>
2025-07-25 02:20:44 +0330

    update admin.controller.js and admin.routes.js

--- controller/admin.controller.js
+++ controller/admin.controller.js
@@ -1,17 +1,19 @@
-import UserModel from "../models/userModel.js"
+import CategoryModel from '../models/category.model.js'
+import UserModel from '../models/userModel.js'
 
+//  ========> get all users controller <======== //
 const getAllUsersController = async (req, res) => {
   try {
-        const users = await UserModel.find()
-        const totalUser = await UserModel.countDocuments()
+    const users = await UserModel.find()
+    const totalUser = await UserModel.countDocuments()
 
-        res.status(200).json({
-                error:false ,
-                success: true ,
-                message:'successfully get all users' ,
-                data:users ,
-                counter:totalUser
-        })
+    res.status(200).json({
+      error: false,
+      success: true,
+      message: 'successfully get all users',
+      data: users,
+      counter: totalUser,
+    })
   } catch (error) {
     return res.status(500).json({
       message: `server error ${error}`,
@@ -21,4 +23,45 @@ const getAllUsersController = async (req, res) => {
   }
 }
 
-export {getAllUsersController}
+//  ========> upload category controller <======== //
+const uploadCategoryController = async (req, res) => {
+  try {
+    const { name, image } = req.body
+    if (!name || !image) {
+      return res.status(400).json({
+        error: true,
+        success: false,
+        message: 'Enter All Required Fieldes',
+      })
+    }
+
+    const addCategory = new CategoryModel({
+      name,
+      image,
+    })
+    const saveCategory = await addCategory.save()
+
+    if(!saveCategory){
+      return res.status(500).json({
+        error: true,
+        success: false,
+        message: 'Not Created',
+      })
+    }
+
+    return res.status(201).json({
+      error: false,
+      success: true,
+      message: 'Successfully Added New Category',
+      data: saveCategory,
+    })
+  } catch (error) {
+    return res.status(500).json({
+      message: `server error ${error}`,
+      error: true,
+      success: false,
+    })
+  }
+}
+
+export { getAllUsersController, uploadCategoryController }
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import Admin from '../middlewares/Admin.js'
-import { getAllUsersController } from '../controller/admin.controller.js'
+import { getAllUsersController, uploadCategoryController } from '../controller/admin.controller.js'
 import verifyToken from '../middlewares/verifyToken.js'
 
 
 const adminRouter = express.Router()
 
 adminRouter.get('/all-users' ,verifyToken, Admin, getAllUsersController)
+adminRouter.post('/upload-category' ,verifyToken, Admin, uploadCategoryController)
 
 export default adminRouter
